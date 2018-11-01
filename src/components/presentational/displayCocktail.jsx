1219e26f1e19ba3e0715d88cb4b45b8609ac7dbf
PGalicia <galicia.patrick@gmail.com>
2018-10-31 21:32:03 -0700

    Setup new presentational component

---
+++ src/components/presentational/displayCocktail.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../style/displayCocktail.css";
+
+const DisplayCocktail = props => (
+  <React.Fragment>
+    <p>Display Container Works</p>
+  </React.Fragment>
+);
+
+export default DisplayCocktail;
