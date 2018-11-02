eb7e0b18716d852adb2ea994d495d130f8cba613
PGalicia <galicia.patrick@gmail.com>
2018-11-01 21:24:18 -0700

    Implemented a 404 page

--- src/components/presentational/main.jsx
+++ src/components/presentational/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import DisplayCocktail from "./displayCocktail";
 import Home from "../container/home";
+import NotFound from "../presentational/notFound";
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/cocktaildb" component={DisplayCocktail} />
+      <Route component={NotFound} />
     </Switch>
   </main>
 );
---
+++ src/components/presentational/notFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => (
+  <React.Fragment>
+    <p>404 Page Not Found</p>
+  </React.Fragment>
+);
+
+export default NotFound;
