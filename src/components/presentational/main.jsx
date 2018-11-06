6ac275b627d4c58ba3a1f77153dbbbdccbdf4af2
PGalicia <galicia.patrick@gmail.com>
2018-11-05 20:35:25 -0800

    Fix path issue

--- src/components/presentational/main.jsx
+++ src/components/presentational/main.jsx
@@ -4,12 +4,13 @@ import DisplayCocktail from "./displayCocktail";
 import Home from "../container/home";
 import NotFound from "../presentational/notFound";
 import "../../style/main.css";
+import HOMEPATH from "../../constants/home-path";
 
 const Main = () => (
   <main className="main">
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/cocktaildb" component={DisplayCocktail} />
+      <Route exact path={HOMEPATH} component={Home} />
+      <Route path={`${HOMEPATH}cocktaildb`} component={DisplayCocktail} />
       <Route component={NotFound} />
     </Switch>
   </main>
