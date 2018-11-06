3bdd9d311e4907345e2fe9793179c863bf811f88
PGalicia <galicia.patrick@gmail.com>
2018-11-05 20:56:40 -0800

    fix bugs

--- src/components/presentational/header.jsx
+++ src/components/presentational/header.jsx
@@ -8,10 +8,10 @@ const Header = () => (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to={process.env.PUBLIC_URL + "/"}>Home</Link>
         </li>
         <li>
-          <Link to="/cocktaildb">Cocktails</Link>
+          <Link to={process.env.PUBLIC_URL + "/cocktaildb"}>Cocktails</Link>
         </li>
       </ul>
     </nav>
