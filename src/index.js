1854906f1d72c1c93e74b9e4e391e03db3d8c3d7
PGalicia <galicia.patrick@gmail.com>
2018-11-05 21:05:13 -0800

    fix react-router with ghpages

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>,
