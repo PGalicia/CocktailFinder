a40d296dac1ed9addf2c72eef0d0d4b6eacd4b3b
PGalicia <galicia.patrick@gmail.com>
2018-11-22 14:50:21 -0800

    collapsible working

--- src/components/presentational/filterChecklist.jsx
+++ src/components/presentational/filterChecklist.jsx
@@ -6,10 +6,24 @@ class FilterChecklist extends Component {
     isExpanded: true
   };
 
+  constructor() {
+    super();
+
+    this.toggleCollapsible = this.toggleCollapsible.bind(this);
+  }
+
+  toggleCollapsible() {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  }
+
   render() {
     return (
       <div className="collapsible">
-        <div className="filter-title" id={this.props.category}>
+        <div
+          className="filter-title"
+          id={this.props.category}
+          onClick={this.toggleCollapsible}
+        >
           <h2>{this.props.name}</h2>
           <span>
             {this.state.isExpanded
@@ -17,25 +31,27 @@ class FilterChecklist extends Component {
               : String.fromCharCode(8722)}
           </span>
         </div>
-        {/* <div className="filter-list">
-          {props.checkboxes
-            .filter(checkbox => checkbox.category === props.category)
-            .map(checkbox => {
-              return (
-                <label className="switch" key={checkbox.id}>
-                  <input
-                    id={checkbox.id}
-                    type="checkbox"
-                    value={checkbox.isChecked}
-                    name={checkbox.name}
-                    checked={checkbox.isChecked}
-                    onChange={props.handleChange}
-                  />
-                  <span className="toggle">{checkbox.name}</span>
-                </label>
-              );
-            })}
-        </div> */}
+        {!this.state.isExpanded && (
+          <div className="filter-list">
+            {this.props.checkboxes
+              .filter(checkbox => checkbox.category === this.props.category)
+              .map(checkbox => {
+                return (
+                  <label className="switch" key={checkbox.id}>
+                    <input
+                      id={checkbox.id}
+                      type="checkbox"
+                      value={checkbox.isChecked}
+                      name={checkbox.name}
+                      checked={checkbox.isChecked}
+                      onChange={this.props.handleChange}
+                    />
+                    <span className="toggle">{checkbox.name}</span>
+                  </label>
+                );
+              })}
+          </div>
+        )}
       </div>
     );
   }
