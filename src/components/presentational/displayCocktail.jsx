f7c83e474508819cf69f0057fa25c8098c1c49c2
PGalicia <galicia.patrick@gmail.com>
2018-12-23 14:40:38 -0800

    Bug Fix: Cocltail Cards  is now displayed horizontally

--- src/components/presentational/displayCocktail.jsx
+++ src/components/presentational/displayCocktail.jsx
@@ -9,13 +9,20 @@ import "../../style/displayCocktail.css";
 import CocktailCard from "../presentational/cocktailCard";
 
 const calculateChunks = (arr, col) => {
-  const chunk = Math.ceil(arr.length / col);
-  // return chunk;
   let result = [];
+  let index = 0;
+  let x = 0;
+  let y = null;
 
-  for (let i = 0; i < arr.length; i += chunk) {
-    let myChunk = arr.slice(i, i + chunk);
-    result.push(myChunk);
+  for (let i = 1; i <= col; i++) {
+    result.push([]);
+  }
+
+  while (x < arr.length) {
+    y = x + 1;
+    result[index].push(arr.slice(x, y)[0]);
+    index = (index + 1) % result.length;
+    x += 1;
   }
 
   return result;
@@ -25,15 +32,10 @@ const DisplayCocktail = props => {
   return (
     <div className="cocktail-container">
       {props.name !== "n/a" ? <h4>{props.name}</h4> : null}
-      {/* <div className="cocktail-list">
-        {props.result.map(cocktail => {
-          return <CocktailCard key={cocktail.name} cocktail={cocktail} />;
-        })}
-      </div> */}
       <div className="cocktail-list">
         {calculateChunks(props.result, 2).map(chunk => {
           return (
-            <div className="cocktail-column">
+            <div className="cocktail-column" key={chunk[0].name}>
               {chunk.map(cocktail => {
                 return <CocktailCard key={cocktail.name} cocktail={cocktail} />;
               })}
