f773d6c1a48e8920831758a229244fc02abbd2d0
PGalicia <galicia.patrick@gmail.com>
2019-01-15 16:52:43 -0800

    Change tab icon

--- src/constants/cocktail.js
+++ src/constants/cocktail.js
@@ -1,6 +1,7 @@
 export const COCKTAILS = [
   {
     name: "Alexander",
+    description: "This is a cool drink!",
     // alcohol: ["Cognac", "Creme De Cacao"],
     alcohol: ["Cognac", "Cacao"],
     ingredients: ["cream"],
@@ -10,238 +11,258 @@ export const COCKTAILS = [
   },
   {
     name: "Americano",
+    description: "This is another cool drink!",
     alcohol: ["Campari", "Vermouth"],
     ingredients: ["soda water"],
     garnish: ["orange"],
     preparation: "Pour the Campari and vermouth over ice into glass, add a splash of soda water and garnish with half orange slice.",
     img: "test-img.png"
-  },
-  {
-    name: "Angel Face",
-    alcohol: ["Gin", "Calvados"],
-    ingredients: ["apricot brandy"],
-    garnish: [],
-    preparation: "Shake all ingredients with ice and strain contents into a cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Aviation",
-    alcohol: ["Gin"],
-    ingredients: ["creme de violette", "lemon juice", "maraschino liqueur"],
-    garnish: ["cherry"],
-    preparation: "Shake all ingredients with ice and strain contents into a cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Bacardi",
-    alcohol: ["Rum"],
-    ingredients: ["lime juice", "grenadine syrup"],
-    garnish: ["lime"],
-    preparation: "	Shake together with ice. Strain into glass and serve.",
-    img: "test-img.png"
-  },
-  {
-    name: "Between The Sheets",
-    alcohol: ["Cognac", "Rum", "Triple Sec"],
-    ingredients: ["lemon juice"],
-    garnish: [],
-    preparation: "Pour all ingredients into shaker with ice cubes, shake, strain into chilled cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Casino",
-    alcohol: ["Gin"],
-    ingredients: ["maraschino", "orange bitters", "lemon juice"],
-    garnish: ["lemon", "maraschino cherry"],
-    preparation: "	Pour all ingredients into shaker with ice cubes. Shake well. Strain into chilled cocktail glass. Garnish with a lemon twist and a maraschino cherry. Serve without a straw.",
-    img: "test-img.png"
-  },
-  {
-    name: "Clover Club",
-    alcohol: ["Gin"],
-    ingredients: ["lemon juice", "raspberry syrup", "egg white"],
-    garnish: [],
-    preparation: "Dry shake ingredients to emulsify, add ice, shake and served straight up.",
-    img: "test-img.png"
-  },
-  {
-    name: "Daiquiri",
-    alcohol: ["Rum"],
-    ingredients: ["lime juice", "simple syrup"],
-    garnish: [],
-    preparation: "Pour all ingredients into shaker with ice cubes. Shake well. Double Strain in chilled cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Derby",
-    alcohol: ["Gin"],
-    ingredients: ["peach bitters", "mint leaves"],
-    garnish: ["mint leaves"],
-    preparation: "Pour all ingredients into a mixing glass with ice. Stir. Strain into a cocktail glass. Garnish with a sprig of fresh mint in the drink.",
-    img: "test-img.png"
-  },
-  {
-    name: "Dry Martini",
-    alcohol: ["Gin"],
-    ingredients: ["vermouth"],
-    garnish: ["olive", "lemon twist"],
-    preparation: "Straight: Pour all ingredients into mixing glass with ice cubes. Stir well. Strain in chilled martini cocktail glass. Squeeze oil from lemon peel onto the drink, or garnish with olive.",
-    img: "test-img.png"
-  },
-  {
-    name: "Gin Fizz",
-    alcohol: ["Gin"],
-    ingredients: ["lemon juice", "gomme syrup", "soda water"],
-    garnish: ["lemon slice"],
-    preparation: "Shake all ingredients with ice cubes, except soda water. Pour into glass. Top with soda water.",
-    img: "test-img.png"
-  },
-  {
-    name: "John Collins",
-    alcohol: ["Gin"],
-    ingredients: ["lemon juice", "sugar syrup", "carbonated water"],
-    garnish: ["lemon slice", "maraschino cherry"],
-    preparation: "Pour all ingredients directly into highball glass filled with ice. Stir gently. Garnish. Add a dash of Angostura bitters.",
-    img: "test-img.png"
-  },
-  {
-    name: "Manhattan",
-    alcohol: ["Whiskey"],
-    ingredients: ["vermouth", "angostura bitters"],
-    garnish: ["maraschino cherry"],
-    preparation: "	Stirred over ice, strained into a chilled glass, garnished, and served up.",
-    img: "test-img.png"
-  },
-  {
-    name: "Mary Pickford",
-    alcohol: ["Rum"],
-    ingredients: ["pineapple juice", "grenadine", "maraschino"],
-    garnish: [],
-    preparation: "Shake and strain into a chilled large cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Monkey Gland",
-    alcohol: ["Gin"],
-    ingredients: ["orange juice", "absinthe", "grenadine"],
-    garnish: [],
-    preparation: "Shake well over ice cubes in a shaker, strain into a chilled cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Negroni",
-    alcohol: ["Gin", "Vermouth", "Campari"],
-    ingredients: [],
-    garnish: ["orange slice"],
-    preparation: "Stir into glass over ice, garnish and serve.",
-    img: "test-img.png"
-  },
-  {
-    name: "Old Fashioned",
-    alcohol: ["Whiskey"],
-    ingredients: ["sugar cube"],
-    garnish: ["orange twist", "cherry"],
-    preparation: "Place sugar cube in old fashioned glass and saturate with bitters, add a dash of plain water. Muddle until dissolved. Fill the glass with ice cubes and add whiskey. Garnish with orange twist, and a cocktail cherry.",
-    img: "test-img.png"    
-  },
-  {
-    name: "Paradise",
-    alcohol: ["Gin"],
-    ingredients: ["apricot brandy", "orange juice"],
-    garnish: [],
-    preparation: "Shake together over ice. Strain into cocktail glass and serve chilled.",
-    img: "test-img.png"
-  },
-  {
-    name: "Planter's Punch",
-    alcohol: ["Rum"],
-    ingredients: ["orange juice", "pineapple juice", "lemon juice", "grenadine syrup", "sugar syrup", "angostura bitters"],
-    garnish: ["cherry", "pineapple slice"],
-    preparation: "Pour all ingredients, except the bitters, into shaker filled with ice. Shake well. Pour into large glass, filled with ice. Add Angostura bitters, 'on top'. Garnish with cocktail cherry and pineapple.",
-    img: "test-img.png"
-  },
-  {
-    name: "Porto Flip",
-    alcohol: ["Port"],
-    ingredients: ["brandy", "egg yolk"],
-    garnish: ["grated nutmeg"],
-    preparation: "Shake ingredients together in a mixer with ice. Strain into glass, garnish and serve",
-    img: "test-img.png"
-  },
-  {
-    name: "Ramos Gin Fizz",
-    alcohol: ["Gin"],
-    ingredients: ["lime juice", "simple syrup", "cream", "egg white", "orange flower water", "vanilla extract", "soda water"],
-    garnish: [],
-    preparation: "All ingredients except the soda are poured in a mixing glass, dry shaken (no ice) for two minutes, then ice is added and shaken hard for another minute",
-    img: "test-img.png"
-  },
-  {
-    name: "Rusty Nail",
-    // alcohol: ["Scotch Whisky"],
-    alcohol: ["Whisky"],
-    ingredients: ["drambuie"],
-    garnish: ["lemon twist"],
-    preparation: "Pour all ingredients directly into old-fashioned glass filled with ice. Stir gently. Garnish with a lemon twist. Serve.",
-    img: "test-img.png"
-  },
-  {
-    name: "Sazerac",
-    alcohol: ["Cognac"],
-    ingredients: ["absinthe", "sugar cube", "peychaud's bitters"],
-    garnish: ["lemon peel"],
-    preparation: "Rinse a chilled old-fashioned glass with the absinthe, add crushed ice, and set it aside. Stir the remaining ingredients over ice and set it aside. Discard the ice and any excess absinthe from the prepared glass, and strain the drink into the glass. Add the lemon peel for garnish",
-    img: "test-img.png"
-  },
-  {
-    name: "Screwdriver",
-    alcohol: ["Vodka"],
-    ingredients: ["orange juice"],
-    garnish: ["orange slice"],
-    preparation: "Mix in a highball glass with ice. Garnish and serve.",
-    img: "test-img.png"
-  },
-  {
-    name: "Sidecar",
-    alcohol: ["Cognac"],
-    ingredients: ["triple sec", "lemon juice"],
-    garnish: [],
-    preparation: "Pour all ingredients into cocktail shaker filled with ice. Shake well and strain into cocktail glass.",
-    img: "test-img.png"
-  },
-  {
-    name: "Stinger",
-    alcohol: ["Cognac"],
-    ingredients: ["white creme de menthe"],
-    garnish: [],
-    preparation: "Pour in a mixing glass with ice, stir and strain into a cocktail glass. May also be served on rocks in a rocks glass.",
-    img: "test-img.png"    
-  },
-  {
-    name: "Tuxedo",
-    alcohol: ["Gin"],
-    ingredients: ["vermouth", "maraschino", "absinthe", "orange bitters"],
-    garnish: ["cherry", "lemon zest"],
-    preparation: "Stir all ingredients with ice and strain into a cocktail glass. Garnish with a cherry and a twist of lemon zest.",
-    img: "test-img.png"
-  },
-  {
-    name: "Whiskey Sour",
-    alcohol: ["Whiskey"],
-    ingredients: ["lemon juice", "gomme syrup", "egg white"],
-    garnish: ["maraschino cherry", "orange slice"],
-    preparation: "Shake with ice. Strain into chilled glass, garnish and serve.",
-    img: "test-img.png"
-  },
-  {
-    name: "White Lady",
-    alcohol: ["Gin"],
-    ingredients: ["triple sec", "lemon juice"],
-    garnish: [],
-    preparation: "Add all ingredients into cocktail shaker filled with ice. Shake well and strain into large cocktail glass.",
-    img: "test-img.png"
   }
 ];
+// export const COCKTAILS = [
+//   {
+//     name: "Alexander",
+//     // alcohol: ["Cognac", "Creme De Cacao"],
+//     alcohol: ["Cognac", "Cacao"],
+//     ingredients: ["cream"],
+//     garnish: ["nutmeg"],
+//     preparation: "Shake all ingredients with ice and strain contents into a cocktail glass. Sprinkle nutmeg on top and serve.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Americano",
+//     alcohol: ["Campari", "Vermouth"],
+//     ingredients: ["soda water"],
+//     garnish: ["orange"],
+//     preparation: "Pour the Campari and vermouth over ice into glass, add a splash of soda water and garnish with half orange slice.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Angel Face",
+//     alcohol: ["Gin", "Calvados"],
+//     ingredients: ["apricot brandy"],
+//     garnish: [],
+//     preparation: "Shake all ingredients with ice and strain contents into a cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Aviation",
+//     alcohol: ["Gin"],
+//     ingredients: ["creme de violette", "lemon juice", "maraschino liqueur"],
+//     garnish: ["cherry"],
+//     preparation: "Shake all ingredients with ice and strain contents into a cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Bacardi",
+//     alcohol: ["Rum"],
+//     ingredients: ["lime juice", "grenadine syrup"],
+//     garnish: ["lime"],
+//     preparation: "	Shake together with ice. Strain into glass and serve.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Between The Sheets",
+//     alcohol: ["Cognac", "Rum", "Triple Sec"],
+//     ingredients: ["lemon juice"],
+//     garnish: [],
+//     preparation: "Pour all ingredients into shaker with ice cubes, shake, strain into chilled cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Casino",
+//     alcohol: ["Gin"],
+//     ingredients: ["maraschino", "orange bitters", "lemon juice"],
+//     garnish: ["lemon", "maraschino cherry"],
+//     preparation: "	Pour all ingredients into shaker with ice cubes. Shake well. Strain into chilled cocktail glass. Garnish with a lemon twist and a maraschino cherry. Serve without a straw.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Clover Club",
+//     alcohol: ["Gin"],
+//     ingredients: ["lemon juice", "raspberry syrup", "egg white"],
+//     garnish: [],
+//     preparation: "Dry shake ingredients to emulsify, add ice, shake and served straight up.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Daiquiri",
+//     alcohol: ["Rum"],
+//     ingredients: ["lime juice", "simple syrup"],
+//     garnish: [],
+//     preparation: "Pour all ingredients into shaker with ice cubes. Shake well. Double Strain in chilled cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Derby",
+//     alcohol: ["Gin"],
+//     ingredients: ["peach bitters", "mint leaves"],
+//     garnish: ["mint leaves"],
+//     preparation: "Pour all ingredients into a mixing glass with ice. Stir. Strain into a cocktail glass. Garnish with a sprig of fresh mint in the drink.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Dry Martini",
+//     alcohol: ["Gin"],
+//     ingredients: ["vermouth"],
+//     garnish: ["olive", "lemon twist"],
+//     preparation: "Straight: Pour all ingredients into mixing glass with ice cubes. Stir well. Strain in chilled martini cocktail glass. Squeeze oil from lemon peel onto the drink, or garnish with olive.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Gin Fizz",
+//     alcohol: ["Gin"],
+//     ingredients: ["lemon juice", "gomme syrup", "soda water"],
+//     garnish: ["lemon slice"],
+//     preparation: "Shake all ingredients with ice cubes, except soda water. Pour into glass. Top with soda water.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "John Collins",
+//     alcohol: ["Gin"],
+//     ingredients: ["lemon juice", "sugar syrup", "carbonated water"],
+//     garnish: ["lemon slice", "maraschino cherry"],
+//     preparation: "Pour all ingredients directly into highball glass filled with ice. Stir gently. Garnish. Add a dash of Angostura bitters.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Manhattan",
+//     alcohol: ["Whiskey"],
+//     ingredients: ["vermouth", "angostura bitters"],
+//     garnish: ["maraschino cherry"],
+//     preparation: "	Stirred over ice, strained into a chilled glass, garnished, and served up.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Mary Pickford",
+//     alcohol: ["Rum"],
+//     ingredients: ["pineapple juice", "grenadine", "maraschino"],
+//     garnish: [],
+//     preparation: "Shake and strain into a chilled large cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Monkey Gland",
+//     alcohol: ["Gin"],
+//     ingredients: ["orange juice", "absinthe", "grenadine"],
+//     garnish: [],
+//     preparation: "Shake well over ice cubes in a shaker, strain into a chilled cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Negroni",
+//     alcohol: ["Gin", "Vermouth", "Campari"],
+//     ingredients: [],
+//     garnish: ["orange slice"],
+//     preparation: "Stir into glass over ice, garnish and serve.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Old Fashioned",
+//     alcohol: ["Whiskey"],
+//     ingredients: ["sugar cube"],
+//     garnish: ["orange twist", "cherry"],
+//     preparation: "Place sugar cube in old fashioned glass and saturate with bitters, add a dash of plain water. Muddle until dissolved. Fill the glass with ice cubes and add whiskey. Garnish with orange twist, and a cocktail cherry.",
+//     img: "test-img.png"    
+//   },
+//   {
+//     name: "Paradise",
+//     alcohol: ["Gin"],
+//     ingredients: ["apricot brandy", "orange juice"],
+//     garnish: [],
+//     preparation: "Shake together over ice. Strain into cocktail glass and serve chilled.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Planter's Punch",
+//     alcohol: ["Rum"],
+//     ingredients: ["orange juice", "pineapple juice", "lemon juice", "grenadine syrup", "sugar syrup", "angostura bitters"],
+//     garnish: ["cherry", "pineapple slice"],
+//     preparation: "Pour all ingredients, except the bitters, into shaker filled with ice. Shake well. Pour into large glass, filled with ice. Add Angostura bitters, 'on top'. Garnish with cocktail cherry and pineapple.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Porto Flip",
+//     alcohol: ["Port"],
+//     ingredients: ["brandy", "egg yolk"],
+//     garnish: ["grated nutmeg"],
+//     preparation: "Shake ingredients together in a mixer with ice. Strain into glass, garnish and serve",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Ramos Gin Fizz",
+//     alcohol: ["Gin"],
+//     ingredients: ["lime juice", "simple syrup", "cream", "egg white", "orange flower water", "vanilla extract", "soda water"],
+//     garnish: [],
+//     preparation: "All ingredients except the soda are poured in a mixing glass, dry shaken (no ice) for two minutes, then ice is added and shaken hard for another minute",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Rusty Nail",
+//     // alcohol: ["Scotch Whisky"],
+//     alcohol: ["Whisky"],
+//     ingredients: ["drambuie"],
+//     garnish: ["lemon twist"],
+//     preparation: "Pour all ingredients directly into old-fashioned glass filled with ice. Stir gently. Garnish with a lemon twist. Serve.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Sazerac",
+//     alcohol: ["Cognac"],
+//     ingredients: ["absinthe", "sugar cube", "peychaud's bitters"],
+//     garnish: ["lemon peel"],
+//     preparation: "Rinse a chilled old-fashioned glass with the absinthe, add crushed ice, and set it aside. Stir the remaining ingredients over ice and set it aside. Discard the ice and any excess absinthe from the prepared glass, and strain the drink into the glass. Add the lemon peel for garnish",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Screwdriver",
+//     alcohol: ["Vodka"],
+//     ingredients: ["orange juice"],
+//     garnish: ["orange slice"],
+//     preparation: "Mix in a highball glass with ice. Garnish and serve.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Sidecar",
+//     alcohol: ["Cognac"],
+//     ingredients: ["triple sec", "lemon juice"],
+//     garnish: [],
+//     preparation: "Pour all ingredients into cocktail shaker filled with ice. Shake well and strain into cocktail glass.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Stinger",
+//     alcohol: ["Cognac"],
+//     ingredients: ["white creme de menthe"],
+//     garnish: [],
+//     preparation: "Pour in a mixing glass with ice, stir and strain into a cocktail glass. May also be served on rocks in a rocks glass.",
+//     img: "test-img.png"    
+//   },
+//   {
+//     name: "Tuxedo",
+//     alcohol: ["Gin"],
+//     ingredients: ["vermouth", "maraschino", "absinthe", "orange bitters"],
+//     garnish: ["cherry", "lemon zest"],
+//     preparation: "Stir all ingredients with ice and strain into a cocktail glass. Garnish with a cherry and a twist of lemon zest.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "Whiskey Sour",
+//     alcohol: ["Whiskey"],
+//     ingredients: ["lemon juice", "gomme syrup", "egg white"],
+//     garnish: ["maraschino cherry", "orange slice"],
+//     preparation: "Shake with ice. Strain into chilled glass, garnish and serve.",
+//     img: "test-img.png"
+//   },
+//   {
+//     name: "White Lady",
+//     alcohol: ["Gin"],
+//     ingredients: ["triple sec", "lemon juice"],
+//     garnish: [],
+//     preparation: "Add all ingredients into cocktail shaker filled with ice. Shake well and strain into large cocktail glass.",
+//     img: "test-img.png"
+//   }
+// ];
 
 // export const COCKTAILS = {
 //   "Alexander": {
