3b6368e466c8a82c857d320e7ed938d9880892f0
PGalicia <galicia.patrick@gmail.com>
2019-05-18 11:56:41 -0700

    Style cocktail card

--- src/js/component/container/home.jsx
+++ src/js/component/container/home.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./../../../scss/home.scss";
 import { connect } from "react-redux";
 import { changeGreeting } from "../../action/index.js";
+import parse from "html-react-parser"; // Parser
+import { COCKTAIL_INFORMATION } from "./../../constant/cocktailsInformation.js";
 
 const mapStateToProps = state => {
   return {
@@ -27,6 +29,31 @@ class Home extends Component {
         <button onClick={() => this.props.changeGreeting("Hi World!")}>
           Change greeting
         </button>
+
+        {COCKTAIL_INFORMATION.map(cocktail => {
+          const { name, description, ingredients, instructions } = cocktail;
+          return (
+            <div key={name} className="cocktail-card-container">
+              <h3>{name}</h3>
+              <p>{description}</p>
+              <div className="cocktail-instructions-container">
+                <h3>How to prepare this drink:</h3>
+                <ul className="ingredients">
+                  {ingredients.map(ingredient => {
+                    return (
+                      <li className="ingredient" key={ingredient.ingredient}>
+                        <span>{ingredient.ingredient}</span>
+                        <span className="dot" />
+                        <span>{ingredient.measurement}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+                {parse(instructions)}
+              </div>
+            </div>
+          );
+        })}
       </>
     );
   }
---
+++ src/js/constant/cocktailsInformation.js
@@ -0,0 +1,15 @@
+export const COCKTAIL_INFORMATION = [
+    {
+        name: "Alexander",
+        description: `A creamy, chocolatey cocktail made with gin. Most will know it's successor: The Brandy Alexander. This is the original.`,
+        ingredients: [
+        { ingredient: "cognac", measurement: "3cl" },
+        { ingredient: "creme de cacao", measurement: "3cl" },
+        { ingredient: "fresh cream", measurement: "3cl" }
+        ],
+        instructions: `
+        <p>Shake and strain into a chilled cocktail glass</p>
+        <p>Sprinkle ground nutmeg</p>
+        `
+  }
+];
